27cb06555a93811df843334086cc4724a5d1c965
Starck43 <saloon.as@gmail.com>
2021-06-23 23:48:39 +0300

    Revert "replace server fetching to static one"
    This reverts commit 23582782b29e8b0a2a17d29d7b1af36180bdc378.

--- components/UI/Header.js
+++ components/UI/Header.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/macro'
 
 const Header = ({ children }) => (
   <header className='mt-4'>
-    <svg className="check-mark svg-icon d-none d-sm-block me-1 me-lg-2"><use xlinkHref="#check-mark-icon"></use></svg>
+    <svg className="check-mark svg-icon me-1 me-lg-2"><use xlinkHref="#check-mark-icon"></use></svg>
     <h1>
       {children}
     </h1>
