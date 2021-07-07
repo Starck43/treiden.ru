c093a7cbabd43667aa69cf6f0c5da963ce1001c5
Starck43 <saloon.as@gmail.com>
2021-07-07 16:00:09 +0300

    changed autoFocus property to true

--- components/UI/Slider.js
+++ components/UI/Slider.js
@@ -28,7 +28,7 @@ const Slider = ({sliders, className, groupKey='', ...sliderProps}) => {
 	return (
 	<Container {...sliderProps} className={className || style.sliderBg}
 		showStatus={false}
-		autoFocus={false}
+		emulateTouch={true}
 		useKeyboardArrows
 		animationHandler="fade"
 		preventMovementUntilSwipeScrollTolerance={true}
