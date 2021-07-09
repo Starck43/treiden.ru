08e4bb05930d82ffd4c58f1d791bb2721a0f2752
Starck43 <saloon.as@gmail.com>
2021-07-09 14:51:37 +0300

    v1.6. Added comments and updated default SEO

--- next.config.js
+++ next.config.js
@@ -13,9 +13,9 @@ module.exports = {
 		logo: '/images/logo.png',
 		bgImage: '/images/bg-space.jpg',
 		meta: {
-			title: 'Трейден Медиа Групп',
-			description: 'Рекламные услуги по продвижению бренда',
-			keywords: "рекламные услуги, маркетинговые услуги",
+			title: 'Трейден Медиа Групп - идеи со знаком качества',
+			description: 'Создание рекламных, коммуникационных и ивент-продуктов федерального качества по провинциальной цене',
+			keywords: "реклама, медиа, брендинг, видеопродакшн, ивенты, AR, дополненная реальность, маркетинговые услуги, продвижение бренда",
 		},
 		googleAnalyticsId: 'G-000000000',
 	},
@@ -24,10 +24,11 @@ module.exports = {
 	//mySecret: 'secret',
 	//secondSecret: process.env.SECOND_SECRET, // Pass through env variables
 	},
+	// settings for next/images
 	images: {
 		domains: [serverName],
-		deviceSizes: [320, 450, 640, 768, 1080, 1200, 1920],
-		imageSizes: [320, 450, 640, 900],
+		deviceSizes: [320, 450, 640, 768, 1080, 1200, 1920], // breakpoints
+		imageSizes: [320, 450, 640, 900], // breakpoints
 	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'styles')],
