493c5f9600c40cfa3cbf67915584b9ae877fa6ed
Starck43 <saloon.as@gmail.com>
2021-07-13 19:30:03 +0300

    Revert "v1.6 Added sitemap and robots.txt"
    This reverts commit 5b92285235663d7367af2b49627e1ffad6978da6.

---
+++ pages/api/events-sitemap.js
@@ -0,0 +1,42 @@
+
+import { SitemapStream, streamToPromise } from 'sitemap'
+
+export default async (req, res) => {
+  try {
+    const smStream = new SitemapStream({
+      hostname: `https://${req.headers.host}`,
+      cacheTime: 600000,
+    });
+
+    // List of events
+    const data = await fetch(process.env.API_SERVER + 'events')
+    const events = await data.json()
+
+    // Create each URL row
+    events.forEach(event => {
+      smStream.write({
+        url: `/event/${event.id}`,
+        changefreq: 'daily',
+        priority: 0.8
+      })
+    })
+
+    // End sitemap stream
+    smStream.end();
+
+    // XML sitemap string
+    const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+    // Change headers
+    res.writeHead(200, {
+      'Content-Type': 'application/xml'
+    });
+
+    // Display output to user
+    res.end(sitemapOutput);
+  } catch(e) {
+    console.log(e)
+    res.send(JSON.stringify(e))
+  }
+
+}
