6d3f6afb57603c7040e5dbe7af6b60e30996b45c
Starck43 <saloon.as@gmail.com>
2022-09-08 18:23:24 +0300

    v 1.7 Fixed warning with viewBox attribute in svg style declaration

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,8 @@ class MyDocument extends Document {
 			<Html lang="ru">
 				<Head/>
 				<body>
-					<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" style={{display:'none'}}>
-						<symbol id="check-mark-icon" viewBox="0 0 46 20">
+					<svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 46 20" style={{display:'none'}}>
+						<symbol id="check-mark-icon">
 							<path d="M1.35987 18.2266L22.75 1.27592L44.1401 18.2266L23.3875 11.2478C22.9739 11.1087 22.5261 11.1087 22.1125 11.2478L1.35987 18.2266Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 						</symbol>
 					</svg>
