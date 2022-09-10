28c9172dae5c692b07335eac6109d773342c61d5
Starck43 <saloon.as@gmail.com>
2022-09-11 00:09:16 +0300

    v 1.7.2 refactored code

--- components/UI/Header.js
+++ components/UI/Header.js
@@ -1,11 +1,9 @@
+import {SvgIcon} from "./Icon"
+
 const Header = ({children}) => (
 	<header className="mb-4">
-		<svg viewBox="0 0 46 20" className="check-mark svg-icon d-none d-sm-block me-1 me-lg-2">
-			<use xlinkHref="#check-mark-icon"/>
-		</svg>
-		<h1>
-			{children}
-		</h1>
+		<SvgIcon id="#check-mark-icon" className={`check-mark d-none d-sm-block me-1 me-lg-2`}/>
+		<h1>{children}</h1>
 	</header>
 )
 
